2c197071f7ca90f868c123b81c16107fa6f09603
Bestmymint <bestmymint@Babyshark.local>
2024-11-02 00:37:06 +0700

    change login page

--- GetSetGoal/static/js/login.js
+++ GetSetGoal/static/js/login.js
@@ -5,10 +5,16 @@ button.addEventListener('mouseover', () => {
     document.querySelectorAll('.effect').forEach(element => {
         element.classList.add('active');
     });
+    document.querySelectorAll('.dev').forEach(element => {
+        element.classList.add('active');
+    });
 })
 
 button.addEventListener('mouseout', () => {
     document.querySelectorAll('.effect').forEach(element => {
+        element.classList.remove('active');
+    });
+    document.querySelectorAll('.dev').forEach(element => {
         element.classList.remove('active');
     });
 })
